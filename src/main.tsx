769f7fa1c9f4cbd11ba072aad3dacd4b38a3b9df
nofarshapir <nofarshapir00@gmail.com>
2025-02-01 13:02:53 +0200

    implement registar and login

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { AddTask } from "./components/AddTask";
 import { Feed } from "./components/Feed";
+import { Register } from "./components/register";
+import { Login } from "./components/Login";
 
 import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: "/add-task",
-    element: <AddTask />,
+    path: "/",
+    element: <Login />,
   },
   {
-    path: "/",
+    path: "/feed",
     element: <Feed />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ToastContainer position="top-right" autoClose={3000} />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
